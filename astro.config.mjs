07033460ebdef2ed289f72f851947d47c713b2a9
Emmanuel Izquierdo <ing.emanuelizq@gmail.com>
2024-09-20 00:59:30 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    base: "./", // Esto hará que las rutas de los assets sean relativas
   },
   // Add your domain name here
   site: "https://lexingtonthemes.com/",
-  integrations: [ sitemap()]
+  integrations: [sitemap()],
 });

c95ea2c58483cc0a75621b0156785f726539aff8
Emmanuel Izquierdo <ing.emanuelizq@gmail.com>
2024-09-23 22:03:26 -0500

    update build and compnent button

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    base: "./", // Esto hará que las rutas de los assets sean relativas
-    build: {
-      assetsDir: "./_astro", // Asegurarte de que las rutas de los assets sean relativas
-      rollupOptions: {
-        output: {
-          entryFileNames: "_astro/[name].js",
-          chunkFileNames: "_astro/[name].js",
-          assetFileNames: "_astro/[name].[ext]",
-        },
-      },
-    },
   },
   // Add your domain name here
   site: "https://lexingtonthemes.com/",
